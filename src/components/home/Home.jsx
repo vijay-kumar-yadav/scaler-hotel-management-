f0f053d96a2d69c3fe6727c79c67ebda65cc86cb
vijay-kumar-yadav <vky405021@gmail.com>
2023-03-02 20:49:22 +0530

    Code optimize

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,123 +1,15 @@
 import React, { useState } from "react";
 import Form from "../form/UserForm";
+import roomData from "../../data/roomData";
+import bookingRoom from "../../data/bookingRoom";
 
 const Home = ({ reservationList, setReservationList }) => {
   const [errorVisible, setErrorVisible] = useState(false);
   const errorMessage = errorVisible ? "This field is required" : null;
 
   const [noOfRoom, setNoOfRoom] = useState(1);
-  let room = {
-    A: { rent: 100, available: 5, roomAvail: [101, 102, 103, 104, 105] },
-    B: { rent: 80, available: 5, roomAvail: [201, 202, 203, 204, 205] },
-    C: { rent: 50, available: 5, roomAvail: [301, 302, 303, 304, 305] },
-  };
-  const [rooms, setRooms] = useState([
-    {
-      id: 101,
-      type: "A",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 102,
-      type: "A",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 103,
-      type: "A",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 104,
-      type: "A",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 105,
-      type: "A",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 201,
-      type: "B",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 202,
-      type: "B",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 203,
-      type: "B",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 204,
-      type: "B",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 205,
-      type: "B",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 301,
-      type: "C",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 302,
-      type: "C",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 303,
-      type: "C",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 304,
-      type: "C",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-    {
-      id: 305,
-      type: "C",
-      bookings: [],
-      status: false,
-      user: { name: "", email: "" },
-    },
-  ]);
+  let room = roomData;
+  const [rooms, setRooms] = useState(bookingRoom);
   const getAvailableRooms = (startTime, endTime, rooms, types) => {
     const availableRooms = [];
     for (let i = 0; i < rooms.length; i++) {
@@ -217,33 +109,30 @@ const Home = ({ reservationList, setReservationList }) => {
     const data = rooms.filter((data) =>
       email === data.user.email ? data.id : ""
     );
-    console.log(data);
     return data;
   }
   return (
-    <>
-      <Form
-        data={data}
-        setData={setData}
-        type={type}
-        setType={setType}
-        userNameAndEmail={userNameAndEmail}
-        setUserNameAndEmail={setUserNameAndEmail}
-        date={date}
-        setDate={setDate}
-        reservationList={reservationList}
-        setReservationList={setReservationList}
-        noOfRoom={noOfRoom}
-        setNoOfRoom={setNoOfRoom}
-        errorMessage={errorMessage}
-        room={room}
-        handleRoomBooking={handleRoomBooking}
-        getAvailableRooms={getAvailableRooms}
-        rooms={rooms}
-        setErrorVisible={setErrorVisible}
-        getRoomBookedDetail={getRoomBookedDetail}
-      />
-    </>
+    <Form
+      data={data}
+      setData={setData}
+      type={type}
+      setType={setType}
+      userNameAndEmail={userNameAndEmail}
+      setUserNameAndEmail={setUserNameAndEmail}
+      date={date}
+      setDate={setDate}
+      reservationList={reservationList}
+      setReservationList={setReservationList}
+      noOfRoom={noOfRoom}
+      setNoOfRoom={setNoOfRoom}
+      errorMessage={errorMessage}
+      room={room}
+      handleRoomBooking={handleRoomBooking}
+      getAvailableRooms={getAvailableRooms}
+      rooms={rooms}
+      setErrorVisible={setErrorVisible}
+      getRoomBookedDetail={getRoomBookedDetail}
+    />
   );
 };
 
---
+++ src/data/roomData.js
@@ -0,0 +1,7 @@
+const roomData = {
+  A: { rent: 100, available: 5, DataAvail: [101, 102, 103, 104, 105] },
+  B: { rent: 80, available: 5, roomAvail: [201, 202, 203, 204, 205] },
+  C: { rent: 50, available: 5, roomAvail: [301, 302, 303, 304, 305] },
+};
+
+export default roomData;
